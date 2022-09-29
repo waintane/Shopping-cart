62057c71455caa1efc49cb0909825be4948aa40a
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-09-29 18:57:00 -0400

    modification du css header augmentation du z-index et adaptabilité

--- src/UI/Carousel.js
+++ src/UI/Carousel.js
@@ -3,13 +3,15 @@ let valueTargetChange = "";
 
     function slideGauche(event){
         let value = event.target.dataset;
+        if(valueTargetChange !== value.container || valueTargetChange === ""){
+            valueCarousel = 0;
+        }
         let target = value.container;
         value.value = valueCarousel;
 
         let containerOffre = document.querySelector(`${target}`);
         let card = document.querySelector(".ShoppingCard_card_container__Qch04");
         let dimension = card.clientWidth;
-        console.log(value.value);
         if(Number(value.value) < 0){
             value.value = Number(value.value) + (dimension + 20);
             valueCarousel = Number(value.value);
@@ -32,7 +34,6 @@ let valueTargetChange = "";
         value.value = valueCarousel;
         value.value = Number(value.value) - (dimension + 20);
         valueCarousel = Number(value.value);
-        console.log(valueCarousel)
 
         containerOffre.style.transform = `translateX(${valueCarousel}px)`;
     }
--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -2,34 +2,39 @@ import CardList from "../components/CardList";
 import classes from "./Accueil.module.css";
 import data from "../DataBase/Data.json";
 import {slideGauche, slideDroite} from "../UI/Carousel"
+import { Link } from "react-router-dom";
 
 function Accueil(){
     let dataHommeVendeur = data.filter(data => data.genre === "homme" && data.vente === "high");
-    let dataFemmeVendeur = data.filter(data => data.genre === "femme" && data.vente === "high")
+    let dataFemmeVendeur = data.filter(data => data.genre === "femme" && data.vente === "high");
 
     let moveCarouselHomme = 0;
     let moveCarouselFemme = 0;
 
     return(
         <div className={classes.main_accueil}>
+            <div>
             <h2>Meilleur vendeur pour homme</h2>
-
+            <Link to="/men-page"><button>Voir plus</button></Link>
+            </div>
             <div className={classes.men_best_seller_container}>
                 <div className={classes.container_carousel}>
                     <button data-value={moveCarouselHomme} data-container={".affichage_offre_homme"} onClick={slideGauche}>g</button>
-                    <button data-value={moveCarouselHomme} data-container={".affichage_offre_homme"} onClick={slideDroite}>d</button>
+                    <button data-value={moveCarouselHomme} data-container={".affichage_offre_homme"} data-length={dataHommeVendeur.length} onClick={slideDroite}>d</button>
                     <div className="affichage_offre_homme">
                     <CardList dataFiltre={dataHommeVendeur}></CardList>
                     </div>
                 </div>
             </div>
 
+            <div>
             <h2>Meilleur vendeur pour femme</h2>
-
+            <Link to="/women-page"><button>Voir plus</button></Link>
+            </div>
             <div className={classes.men_best_seller_container}>
                 <div className={classes.container_carousel}>
                     <button data-value={moveCarouselFemme} data-container={".affichage_offre_femme"} onClick={slideGauche}>g</button>
-                    <button data-value={moveCarouselFemme} data-container={".affichage_offre_femme"} onClick={slideDroite}>d</button>
+                    <button data-value={moveCarouselFemme} data-container={".affichage_offre_femme"} data-length={dataFemmeVendeur.length} onClick={slideDroite}>d</button>
                     <div className="affichage_offre_femme">
                     <CardList dataFiltre={dataFemmeVendeur}></CardList>
                     </div>
